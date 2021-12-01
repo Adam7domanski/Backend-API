2fce529410d71bc9c0e836f00ca8509a13474811
Adam Doamński <s13075@pjwstk.edu.pl>
2021-12-01 13:21:32 +0100

    Initial commit CRYP-4

---
+++ src/main/java/com/crypixie/Crypixie.java
@@ -0,0 +1,13 @@
+package com.crypixie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Crypixie {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Crypixie.class, args);
+    }
+
+}
